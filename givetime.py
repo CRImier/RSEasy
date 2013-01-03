900daff4f245b9f4abec672c7c489624377ac5b9
CRImier <crimier@yandex.ru>
2013-01-03 06:17:28 +0200

    Major issues in givetime.py
    In some cases str wasn't converted to int which leads to completely wrong result.

--- givetime.py
+++ givetime.py
@@ -7,16 +7,16 @@ def givetimes(schedarr, times):
 	mn = now_time.minute
 	commin = (hn * 60) + mn #Minutes from start of day by now
 	try: #Try to ensure you don't fall out of array boundaries
-		while schedarr[x][0]<hn :
+		while int(schedarr[x][0])<hn :
 			x += 1
-		while(schedarr[x][0] * 60) + schedarr[x][1] <= commin : #
+		while(int(schedarr[x][0]) * 60) + int(schedarr[x][1]) <= commin : 
 				x += 1
 	except:
 		print "--:--"
 	else:
 		try:
 			while times>=0:
-				print schedarr[x][0],":",schedarr[x][1]
+				print str(schedarr[x][0])+":"+str(schedarr[x][1])
 				x += 1
 				times -= 1
 		except:
